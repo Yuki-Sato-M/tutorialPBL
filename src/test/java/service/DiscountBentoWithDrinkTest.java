b753bb71eef9ca99318744131bbf3cb770990b3b
Mitsuru Takeda <mitsuru.takeda03@gmail.com>
2021-06-10 14:47:54 +0900

    add DiscountBentoWithDrink

---
+++ src/test/java/service/DiscountBentoWithDrinkTest.java
@@ -0,0 +1,44 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+import payment.Basket;
+import payment.ItemTable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountBentoWithDrinkTest {
+
+    @Test
+    void のり弁当1個とお茶1つで20円割引になる() {
+        Basket basket = new Basket();
+        DiscountBentoWithDrink discountBentoWithDrink = new DiscountBentoWithDrink();
+        basket.add(ItemTable.NORIBEN);
+        basket.add(ItemTable.TEA);
+        int discountPrice = discountBentoWithDrink.getDiscountPrice(basket);
+        assertEquals(20, discountPrice);
+    }
+    @Test
+    void のり弁当1個と鮭弁当1個とお茶1つとコーヒー1個で40円割引になる() {
+        Basket basket = new Basket();
+        DiscountBentoWithDrink discountBentoWithDrink = new DiscountBentoWithDrink();
+        basket.add(ItemTable.NORIBEN);
+        basket.add(ItemTable.SYAKEBEN);
+        basket.add(ItemTable.COFFEE);
+        basket.add(ItemTable.TEA);
+
+        int discountPrice = discountBentoWithDrink.getDiscountPrice(basket);
+        assertEquals(40, discountPrice);
+    }
+
+    @Test
+    void のり弁当1個と鮭弁当1個とお茶1つで20円割引になる() {
+        Basket basket = new Basket();
+        DiscountBentoWithDrink discountBentoWithDrink = new DiscountBentoWithDrink();
+        basket.add(ItemTable.NORIBEN);
+        basket.add(ItemTable.SYAKEBEN);
+        basket.add(ItemTable.TEA);
+
+        int discountPrice = discountBentoWithDrink.getDiscountPrice(basket);
+        assertEquals(20, discountPrice);
+    }
+}
