f126a29528628e0f9544c073b1575c7e3db7b04e
Honami Furukawa <h.furukawa3141@gmail.com>
2021-06-10 13:33:31 +0900

    対話的にお買い物ができる

--- src/main/java/consoledemo/Main.java
+++ src/main/java/consoledemo/Main.java
@@ -2,9 +2,7 @@ package consoledemo;
 
 public class Main {
     public static void main(String[] args) {
-        DisplayMenu menu = new DisplayMenu();
-        menu.init();
-        menu.input();
-        menu.calc();
+        Shopping shopping = new Shopping();
+        shopping.run();
     }
 }
--- src/main/java/consoledemo/Shopping.java
+++ src/main/java/consoledemo/Shopping.java
@@ -5,32 +5,58 @@ import payment.Cashier;
 import payment.ItemTable;
 
 
-public class DisplayMenu {
+public class Shopping {
     private Cashier cashier;
     private Basket basket;
 
-    DisplayMenu() {
+    Shopping() {
         cashier = new Cashier();
         basket = new Basket();
     }
 
-    public void init() {
+    public void run() {
+        //
+        Input input = new Input();
+
+        while (true) {
+            displayMenu();
+            input();
+            calc();
+
+
+        }
+    }
+
+    public void displayMenu() {
         System.out.println("====================");
+        System.out.println("0 : 購入を終了する");
         for (ItemTable itemTable : ItemTable.values()) {
             System.out.println(itemTable);
         }
+
         System.out.println("====================");
     }
 
     public void input() {
         Input input = new Input();
         int itemNum = input.getInt("商品番号を入力してください");
+        if (isEnd(itemNum)) {
+            System.exit(0);
+        }
         int itemAmount = input.getInt("購入個数を入力してください");
         //計算する
         addItem(itemNum, itemAmount);
     }
 
-    public void addItem(int itemNum, int itemAmount){
+    public boolean isEnd(int itemNum) {
+        if (itemNum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addItem(int itemNum, int itemAmount) {
         for (ItemTable itemTable : ItemTable.values()) {
             if (itemNum == itemTable.getItem().getId()) {
                 basket.add(itemTable, itemAmount);
