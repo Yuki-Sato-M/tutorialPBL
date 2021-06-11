9a1cb68e028643a982c5ab906bcdc5aa508ba03d
Tobaru Nozomi <nozomi.tobaru.sb2021@gmail.com>
2021-06-11 11:03:11 +0900

    [add] add, mul to price

--- src/main/java/payment/Price.java
+++ src/main/java/payment/Price.java
@@ -9,6 +9,21 @@ public class Price {
 
     @Override
     public String toString() {
-        return String.format("%,3d",value) + "円";
+        return String.format("%,3d", value) + "円";
+    }
+
+    public Price add(Price price) {
+        return new Price(this.getValue() + price.getValue());
+    }
+    //使用方法
+    //Price total
+    //total　= total.add(計算結果)
+
+    public Price mul(int num) {
+        return new Price(this.getValue() * num);
+    }
+
+    public int getValue() {
+        return value;
     }
 }
